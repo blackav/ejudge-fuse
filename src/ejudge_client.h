7dd432f7f12c2c4cabbf8da0acbfcee47f363b1b
Alexander Chernov <blackav@gmail.com>
2018-05-18 03:23:32 +0300

    refactoring

--- src/ejudge_client.h
+++ src/ejudge_client.h
@@ -5,6 +5,15 @@ struct EjContestState;
 struct EjProblemInfo;
 struct EjSessionValue;
 struct EjContestInfo;
+struct EjContestSession;
+
+void
+ejudge_client_enter_contest_request(
+        struct EjFuseState *ejs,
+        struct EjContestState *ecs,
+        struct EjContestSession *ecc,
+        const unsigned char *session_id,
+        const unsigned char *client_key);
 
 void
 ejudge_client_contest_info_request(
